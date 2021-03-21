1b9ae1858d2918025195a5b4a36f90d39875c2ac
Chad <wehbe.chady@gmail.com>
2021-03-21 18:52:49 +0200

    - Added the db and .idea to gitignore. - Added Composer dependencies. - Created the DB for the todo list and its categories (migration). - Created the CRUD interfaces for Todo and Category.

---
+++ src/Entity/Todo.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="todos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $todotext;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getTodotext(): ?string
+    {
+        return $this->todotext;
+    }
+
+    public function setTodotext(string $todotext): self
+    {
+        $this->todotext = $todotext;
+
+        return $this;
+    }
+}
